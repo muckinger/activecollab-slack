b1365f551fb0300a250247d66a2c00e3e4f23dd8
Marco Raddatz <info@marcoraddatz.com>
2015-07-09 21:53:31 +0200

    Use bigger AC icon for mobile support

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function slack_post_message($channel, $message) {
         'text'          => $message,
         'username'      => 'ActiveCollab',
         'as_user'       => FALSE,
-        'icon_url'      => defined('ASSETS_URL') ? ASSETS_URL . '/images/system/default/application-branding/logo.40x40.png'  : '',
+        'icon_url'      => defined('ASSETS_URL') ? ASSETS_URL . '/images/system/default/application-branding/logo.80x80.png'  : '',
         'link_names'    => 1
     ));
 }
